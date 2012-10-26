7b7790311c4ad1e35512df1ac6a11fb561607785
Christian Brel <ch.brel@gmail.com>
2012-10-26 17:27:18 +0200

    Fixed reset password bug for depositors.

--- app/models/depositor.rb
+++ app/models/depositor.rb
@@ -29,6 +29,5 @@ class Depositor < User
   has_one :address, :dependent => :destroy, :foreign_key => 'user_id'
   has_many :items, :dependent => :destroy, :foreign_key => 'user_id'
   
-  validates :email, :confirmation => true
-  validates :email_confirmation, :presence => true
+  validates :email, :presence => true
 end
